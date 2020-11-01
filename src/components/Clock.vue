319b3a7253d0d50b356e44d74a455d6da7433566
cchampou <clement@champouillon.com>
2020-11-01 01:29:39 +0100

    Some cleaning and a brand new clock on the page !

---
+++ src/components/Clock.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>{{ hours }} : {{ minutes }} : {{ seconds }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'Clock',
+  data: () => ({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  }),
+  mounted() {
+    this.updateTime();
+    this.timer = setInterval(this.updateTime, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+  },
+  methods: {
+    updateTime() {
+      const currentDateTime = new Date();
+      this.hours = ('0' + currentDateTime.getHours()).slice(-2);
+      this.minutes = ('0' + currentDateTime.getMinutes()).slice(-2);
+      this.seconds = ('0' + currentDateTime.getSeconds()).slice(-2);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
